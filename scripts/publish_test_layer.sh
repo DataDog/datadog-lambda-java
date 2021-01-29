7e721313d91c130f4f9735d87eedd0b46e20f4d9
chris.agocs <chris.agocs@datadoghq.com>
2021-01-29 15:43:11 -0500

    Layer signing

--- scripts/publish_test_layer.sh
+++ scripts/publish_test_layer.sh
@@ -13,12 +13,24 @@ set -e
 # Makes sure any subprocesses will be terminated with this process
 trap "pkill -P $$; exit 1;" INT
 
-LATEST_JAR_URL=$(curl -i https://repository.sonatype.org/service/local/artifact/maven/redirect\?r\=central-proxy\&g\=com.datadoghq\&a\=dd-java-agent\&v\=LATEST | grep location | cut -d " " -f 2)
+if [ -z "$1" ]
+then
+  echo "Usage: publish_test_layer path/to/layerFile.jar [region]"
+  exit 0
+fi
 
-LATEST_JAR_FILE=$(echo $LATEST_JAR_URL | rev | cut -d "/" -f 1 | rev)
+
+LAYER_FILE=$1
+REGION=$2
+LAYER_ZIP="tracer.zip"
+
+if [ -z "$REGION" ]
+then
+  REGION="sa-east-1"
+fi
 
 echo ""
-echo "About to publish a new Lambda layer containing $LATEST_JAR_FILE"
+echo "About to publish a TEST Lambda layer containing $LAYER_FILE to $REGION"
 read -r -p "Continue? [y/N] " response
 if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
@@ -26,55 +38,36 @@ then
    exit 0
 fi
 
-wget -O $LATEST_JAR_FILE https://dtdg.co/latest-java-tracer
-
-LAYER_ZIP="tracer.zip"
-
 rm -rf layer
 mkdir -p layer/java/lib
-cp "$LATEST_JAR_FILE" layer/java/lib/dd-java-agent.jar
+cp "$LAYER_FILE" layer/java/lib/dd-java-agent.jar
 cd layer
 zip -r ../$LAYER_ZIP java
 cd ..
 rm -rf layer
 
-# sign the layer zip
-
 echo
-echo "Signing layers..."
-./scripts/sign_layers.sh prod $LAYER_ZIP
-
+echo "Signing layer..."
+./scripts/sign_layers.sh sandbox $LAYER_ZIP
 
 AVAILABLE_REGIONS=$(aws ec2 describe-regions --output json | jq -r '.[] | .[] | .RegionName')
 
-
-# Check region arg
-if [ -z "$1" ]; then
-    echo "Region parameter not specified, running for all available regions."
-    REGIONS=$AVAILABLE_REGIONS
-else
-    echo "Region parameter specified: $1"
-    if [[ ! "$AVAILABLE_REGIONS" == *"$1"* ]]; then
-        echo "Could not find $1 in available regions: $AVAILABLE_REGIONS"
-        echo ""
-        echo "EXITING SCRIPT."
-        exit 1
-    fi
-    REGIONS=($1)
+echo "Region parameter specified: $REGION"
+if [[ ! "$AVAILABLE_REGIONS" == *"$REGION"* ]]; then
+    echo "Could not find $REGION in available regions: $AVAILABLE_REGIONS"
+    echo ""
+    echo "EXITING SCRIPT."
+    exit 1
 fi
-
-echo "Starting publishing layers for regions: $REGIONS"
-
 
 publish_layer() {
     region=$1
-    layer_name="dd-trace-java"
-    aws_version_key=$3
-    layer_path=$4
+    layer_name="dd-trace-java-test"
+    aws_version_key=$2
     version_nbr=$(aws lambda publish-layer-version --layer-name $layer_name \
         --description "Datadog Tracer Lambda Layer for Java" \
-        --zip-file "fileb://tracer.zip" \
-        --region $region \
+        --zip-file "fileb://$LAYER_ZIP" \
+        --region "$region" \
 	--output json \
                         | jq -r '.Version')
 
@@ -88,12 +81,8 @@ publish_layer() {
     echo "Published layer for region $region, layer_name $layer_name, layer_version $version_nbr"
 }
 
-for region in $REGIONS
-do
-    echo "Starting publishing layer for region $region..."
+echo "Starting publishing layer for region $REGION..."
 
-    publish_layer $region $layer_name $aws_version_key
-done
-
+publish_layer $REGION "$aws_version_key"
 
 echo "Done !"
