8bb801b1eaae40416462698731a74892bfc52842
chris.agocs <chris.agocs@datadoghq.com>
2021-08-17 10:59:51 -0400

    tests should run with docker rm

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 # The stock gradle:7.1.0-jdk8 docker image has everything we need to build this project.
 # No need for a Dockerfile
-docker run --name datadog-lambda-java-tester \
+docker run --rm \
   -v $(pwd):/datadog-lambda-java \
   -w /datadog-lambda-java \
   gradle:7.1.0-jdk8 \
